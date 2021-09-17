3448242b834b053026e659a3bbd789ca3aa27ddb
Treveon Edwards <fasuke322@gmail.com>
2021-09-16 23:29:15 -0400

    made changes

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./App.css";
+import Header from '../header';
+
+function App() {
+    return (
+        <div className="app">
+            <h1>Hello youtube clone</h1>
+
+            <Header />
+
+            {/* Header*/}
+            {/*/sidebar */}
+            {/*/ reccomend*/}
+        </div>
+
+    );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MenuIcon from "@material-ui/icons/Menu"
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>this is a header</h1>
+            <MenuIcon />  
+        </div>
+    )
+}
+
+export default Header;
